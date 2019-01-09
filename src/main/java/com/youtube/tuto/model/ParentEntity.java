e15886299534cd5084db31042ab8d89802ba99ca
RoberPerfetto <rober_perfe@hotmail.com>
2019-01-09 18:26:20 -0300

    Entity user created

---
+++ src/main/java/com/youtube/tuto/model/ParentEntity.java
@@ -0,0 +1,32 @@
+package com.youtube.tuto.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+@Access(AccessType.FIELD) //Pone las anotaciones sobre el campo y no sobre la propiedad
+public class ParentEntity implements Serializable{
+
+	private static final long serialVersionUID = -1077518354020591852L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="id", unique=true, nullable=false)
+	private Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+}
